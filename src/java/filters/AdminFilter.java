b683448976a86f9f44772218f5819895af884e92
652343 <652343@ELF8IT221633.ACDM.DS.SAIT.CA>
2019-11-03 08:40:22 -0700

    Finished Lab

--- src/java/filters/AdminFilter.java
+++ src/java/filters/AdminFilter.java
@@ -15,16 +15,17 @@ import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
-import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import models.Role;
+import models.User;
+import services.UserService;
 
 /**
  *
  * @author 652343
  */
-@WebFilter(filterName = "AdminFilter", servletNames =
-  {
-    "UserServlet"
-  })
 public class AdminFilter implements Filter
   {
 
@@ -33,16 +34,44 @@ public class AdminFilter implements Filter
             throws IOException, ServletException
       {
 
+        HttpServletRequest hsr = (HttpServletRequest) request;
+        HttpSession session = hsr.getSession();
+
+        String email = (String) session.getAttribute("email");
+
+        UserService us = new UserService();
+
+        try
+          {
+            User u = us.get(email);
+            Role r = u.getRole();
+
+            String roleName = r.getRoleName();
+
+            if (!roleName.contains("admin"))
+              {
+                HttpServletResponse hsre = (HttpServletResponse) response;
+                hsre.sendRedirect("home");
+                return;
+              }
+          } catch (Exception ex)
+          {
+
+          }
+
         chain.doFilter(request, response);
-
       }
 
-    public void destroy()
-      {
+  
+public void destroy()
+      {        
       }
 
+    
     public void init(FilterConfig filterConfig)
-      {
-
+      {        
+       
       }
+
+    
   }
